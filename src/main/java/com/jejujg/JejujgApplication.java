eccd646f5a8d2920bd530ec90d3e5a0fecea8dbb
times132 <times132@gmail.com>
2020-06-01 15:21:17 +0900

    setup

---
+++ src/main/java/com/jejujg/JejujgApplication.java
@@ -0,0 +1,13 @@
+package com.jejujg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejujgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JejujgApplication.class, args);
+    }
+
+}
